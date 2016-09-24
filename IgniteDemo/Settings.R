3afe9713d1d8a8a8661bef5d2899b373e4320daa
John Lam <jlam@iunknown.com>
2016-09-23 17:14:38 -0700

    Adding a .rmarkdown file that contains a unit test that validates that the database is installed correctly and that RevoScaleR and RODBC are installed and functional

---
+++ IgniteDemo/Settings.R
@@ -0,0 +1,7 @@
+# Application settings file.
+# File content was generated on 9/23/2016 3:05:29 PM.
+
+# [Category] SQL
+# [Description] Database connection string
+# [Editor] ConnectionStringEditor
+dbConnection <- 'Driver={SQL Server};Server=(local);Database=NYCTaxiData;Trusted_Connection=yes'
---
+++ IgniteDemo/dependencies.R
@@ -0,0 +1,16 @@
+install_if_not_present <- function(name) {
+    if (!(name %in% rownames(installed.packages()))) {
+        install.packages(name)
+    }
+}
+
+# These are the mandatory dependencies in this project
+
+install_if_not_present("RODBC")
+install_if_not_present("RUnit")
+
+# Assert that we are running Microsoft R Client 3.2.5
+
+if (!"RevoScaleR" %in% rownames(installed.packages())) {
+    print("You need to install Microsoft R Client from http://aka.ms/rclient/download")
+}
